54451fc78fd240e2530e220266c7ff9759422db5
Victor Sumner <victor.sumner@desire2learn.com>
2013-07-31 14:40:43 -0400

    added some default styling, added some default js from button

--- link.js
+++ link.js
@@ -1,1 +1,59 @@
-﻿
+﻿(function ($) {
+
+    $.widget('vui.vui_link', {
+
+        _create: function () {
+
+            var $node = $(this.element);
+
+            var offscreenText = $node.children('.vui-offscreen').text();
+            var title = $node.prop('title');
+            var hasTitle = title !== undefined && title.length > 0;
+
+            if (offscreenText.length > 0 && !hasTitle) {
+                this.element.data('autoUpdateTitleWithText', true);
+                $node.prop('title', offscreenText);
+            }
+
+        },
+
+        getText: function () {
+
+            var $node = $(this.element);
+
+            var text;
+
+            text = $node.text().trim();
+            return text;
+
+        },
+
+        setText: function (text) {
+
+            if (this.element.data('autoUpdateTitleWithText')) {
+                this.element.prop('title', text);
+            }
+
+            var offscreen = this.element.children('.vui-offscreen');
+            if (offscreen.length === 1) {
+                $(offscreen[0]).text(text);
+                return;
+            }
+
+            this.element.contents().filter(function () {
+                return this.nodeType === 3;
+            }).remove();
+            this.element.append(document.createTextNode(text));
+
+        }
+
+    });
+
+    vui.addClassInitializer(
+			'vui-link',
+			function (node) {
+                $(node).vui_link();
+			}
+		);
+
+})(window.jQuery);
