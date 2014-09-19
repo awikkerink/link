64abb7220ff592a9cd917492634cd734b1d6672b
Dave Batiste <dave.batiste@d2l.com>
2014-09-19 14:50:41 -0400

    Adding link unit tests.

--- test/unit/linkSpec.js
+++ test/unit/linkSpec.js
@@ -6,18 +6,55 @@
 		var node;
 
 		beforeEach( function () {
-			node = document.body.appendChild( document.createElement( 'div' ) );
+
+			jasmine.addMatchers( d2l.jasmine.matchers );
+
+			node = document.body.appendChild( document.createElement( 'a' ) );
 			node.className = 'vui-link';
+
 		} );
 
 		afterEach( function() {
+
 			document.body.removeChild( node );
+
 		} );
 
-		it( 'has the correct color', function() {
+		it( 'defines a "vui-link" selector', function() {
+			expect( document ).toHaveCssSelector( '.vui-link' );
+		} );
 
-			var style = window.getComputedStyle( node, null ).getPropertyValue( 'color' );
-			expect( style ).toBe( 'rgb(0, 97, 127)' );
+		it( 'defines a "vui-link:hover" selector', function() {
+			expect( document ).toHaveCssSelector( '.vui-link:hover' );
+		} );
+
+		it( 'defines a "vui-link:focus" selector', function() {
+			expect( document ).toHaveCssSelector( '.vui-link:focus' );
+		} );
+
+		it( 'defines a "vui-link-main" selector', function() {
+			expect( document ).toHaveCssSelector( '.vui-link-main' );
+		} );
+
+		it( 'applies the correct color', function() {
+			expect( node ).toHaveRgbColor( 'rgb(0, 97, 127)' );
+		} );
+
+		it( 'applies text-decoration to not have underline', function() {
+			expect( node ).toHaveTextDecoration( 'none' );
+		} );
+
+		describe( 'main', function() {
+
+			beforeEach( function () {
+
+				node.className = 'vui-link-main';
+
+			} );
+
+			it( 'applies font-weight to be bold', function() {
+				expect( node ).toHaveFontWeight( 'bold' );
+			} );
 
 		} );
 
